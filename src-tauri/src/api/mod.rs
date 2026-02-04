f3b9baf2f1263f85f24272255eb2c1cf86b21d8c
Callum Watkins <callum@callumwatkins.com>
2026-02-04 23:08:14 +0000

    API invoke handler

--- src-tauri/src/api/mod.rs
+++ src-tauri/src/api/mod.rs
@@ -26,3 +26,30 @@ pub async fn hello(name: String) -> Result<String, ApiError> {
     Ok(format!("Hello, {name}!"))
 }
 
+pub fn invoke_handler() -> impl Fn(tauri::ipc::Invoke<tauri::Wry>) -> bool + Send + Sync + 'static {
+    use specta_typescript::{BigIntExportBehavior, Typescript};
+    use tauri_specta::{collect_commands, Builder};
+
+    let builder = Builder::<tauri::Wry>::new().commands(collect_commands![
+        hello
+    ]);
+
+    #[cfg(debug_assertions)]
+    {
+        use std::path::PathBuf;
+
+        let bindings_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("..")
+            .join("app")
+            .join("bindings.ts");
+
+        builder
+            .export(
+                Typescript::default().bigint(BigIntExportBehavior::Number),
+                bindings_path,
+            )
+            .expect("Failed to export typescript bindings");
+    }
+
+    builder.invoke_handler()
+}
